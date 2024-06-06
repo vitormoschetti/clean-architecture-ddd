20bf50b539345be3eb2ce3dc7922441e0b2848e7
Vitor Moschetti <vitormoschetti@terra.com.br>
2024-06-06 20:22:14 -0300

    domain rules

--- src/main/java/br/com/cleanarch/domain/checkout/repository/IOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.cleanarch.domain.checkout.repository;//package br.com.ddd.domain.checkout.repository;
-//
-//import br.com.ddd.domain.checkout.entity.Order;
-//import br.com.ddd.domain.shared.repository.IGenericRepository;
-//
-//public interface IOrderRepository extends IGenericRepository<Order> {
-//}
--- src/main/java/br/com/cleanarch/domain/product/repository/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cleanarch.domain.product.repository;//package br.com.ddd.domain.product.repository;
-//
-//import br.com.ddd.domain.product.entity.Product;
-//import br.com.ddd.domain.shared.repository.IGenericRepository;
-//
-//public interface IProductRepository extends IGenericRepository<Product> {
-//
-//
-//}
