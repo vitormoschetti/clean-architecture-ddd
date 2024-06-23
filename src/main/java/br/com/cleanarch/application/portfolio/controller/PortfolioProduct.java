0ed1238adb98ce3f8cf0be13d46962898c3acee9
Vitor Moschetti <vitormoschetti@terra.com.br>
2024-06-23 12:50:43 -0300

    portfolio controller

--- src/main/java/br/com/cleanarch/application/portfolio/controller/PortfolioProduct.java
+++ src/main/java/br/com/cleanarch/application/portfolio/controller/PortfolioProduct.java
@@ -1,8 +1,8 @@
 package br.com.cleanarch.application.portfolio.controller;
 
-import br.com.cleanarch.application.portfolio.usecase.PortfolioFindItemUseCase;
-import br.com.cleanarch.application.portfolio.usecase.PortfolioFindPortfolioUseCase;
-import br.com.cleanarch.application.portfolio.usecase.PortfolioListItemsUseCase;
+import br.com.cleanarch.application.portfolio.input.PortfolioBuyItemInput;
+import br.com.cleanarch.application.portfolio.input.PortfolioSellItemInput;
+import br.com.cleanarch.application.portfolio.usecase.*;
 import br.com.cleanarch.application.shared.response.Response;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,20 +19,22 @@ public class PortfolioProduct {
     private final PortfolioFindItemUseCase findItemUseCase;
     private final PortfolioFindPortfolioUseCase findPortfolioUseCase;
     private final PortfolioListItemsUseCase listItemsUseCase;
-
-    @GetMapping("/item/{assetId}")
-    public void findItem(@RequestHeader("portfolioId") UUID portfolioId, @PathVariable("assetId") Long assetId) {
-
-        final var response = findItemUseCase.execute(Tuples.of(portfolioId, assetId));
-
-        Response.successResponse(HttpStatus.OK, response);
-
-    }
+    private final PortfolioBuyItemUseCase buyItemUseCase;
+    private final PortfolioSellItemUseCase sellItemUseCase;
 
     @GetMapping
     public void findPortfolio(@RequestHeader("tenantId") UUID tenantId) {
 
         final var response = findPortfolioUseCase.execute(tenantId);
+
+        Response.successResponse(HttpStatus.OK, response);
+
+    }
+
+    @GetMapping("/item/{assetId}")
+    public void findItem(@RequestHeader("portfolioId") UUID portfolioId, @PathVariable("assetId") Long assetId) {
+
+        final var response = findItemUseCase.execute(Tuples.of(portfolioId, assetId));
 
         Response.successResponse(HttpStatus.OK, response);
 
@@ -46,5 +48,24 @@ public class PortfolioProduct {
         Response.successResponse(HttpStatus.OK, response);
 
     }
+
+    @PostMapping("/buy")
+    public void buyItem(@RequestHeader("portfolioId") UUID portfolioId, PortfolioBuyItemInput input) {
+
+        buyItemUseCase.execute(Tuples.of(portfolioId, input));
+
+        Response.successResponse(HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/sell")
+    public void sellItem(@RequestHeader("portfolioId") UUID portfolioId, PortfolioSellItemInput input) {
+
+        sellItemUseCase.execute(Tuples.of(portfolioId, input));
+
+        Response.successResponse(HttpStatus.CREATED);
+
+    }
+
 
 }
