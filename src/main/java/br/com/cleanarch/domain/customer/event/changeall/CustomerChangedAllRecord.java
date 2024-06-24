bed1c5fc2bbc59d029e5755b53203a384abb56d1
Vitor Moschetti <vitormoschetti@terra.com.br>
2024-06-24 14:03:41 -0300

    domain events handlers

--- src/main/java/br/com/cleanarch/domain/customer/event/changeall/CustomerChangedAllRecord.java
+++ src/main/java/br/com/cleanarch/domain/customer/event/changeall/CustomerChangedAllRecord.java
@@ -1,6 +1,5 @@
 package br.com.cleanarch.domain.customer.event.changeall;
 
-import br.com.cleanarch.domain.customer.valueobject.AddressVO;
 import br.com.cleanarch.domain.shared.event.IRecord;
 
 import java.util.UUID;
--- src/main/java/br/com/cleanarch/domain/customer/event/changedaddress/CustomerChangedAddressRecord.java
+++ src/main/java/br/com/cleanarch/domain/customer/event/changedaddress/CustomerChangedAddressRecord.java
@@ -1,6 +1,5 @@
 package br.com.cleanarch.domain.customer.event.changedaddress;
 
-import br.com.cleanarch.domain.customer.valueobject.AddressVO;
 import br.com.cleanarch.domain.shared.event.IRecord;
 
 import java.util.UUID;
